0899745570f4cb7efe1997a273fd4c52a4460894
Kenji Tayama <kenji.tayama@gmail.com>
2014-08-16 11:13:07 +0900

    prevent screen locking

--- MCSimple/EXAppDelegate.m
+++ MCSimple/EXAppDelegate.m
@@ -26,6 +26,7 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [[UIApplication sharedApplication] setIdleTimerDisabled:NO];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -36,6 +37,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[UIApplication sharedApplication] setIdleTimerDisabled:YES];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
